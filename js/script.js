59c0d0c77350384896e4ede0fca49dced7b5d1ad
Charllys de Lima <charllys.lima@outlook.com>
2022-07-14 09:48:03 -0300

    sound jump - responsive adapter

--- js/script.js
+++ js/script.js
@@ -7,11 +7,16 @@ const soundButton = document.querySelector(".sound-control");
 
 const themeAudio = document.querySelector("#theme-song");
 const pipeAudio = document.querySelector("#pipe-sound");
+const jumpAudio = document.querySelector("#jump-sound");
 
 const mario = document.querySelector("#mario");
 const pipeStart = document.querySelector(".pipe-initial");
+const pipe = document.querySelector("#pipe");
+const scores = document.querySelector(".scores");
 
 const jumpSpeed = 250;
+
+let isStarted = false;
 
 root.style.setProperty("--jumpSpeed", `${jumpSpeed}ms`);
 
@@ -19,7 +24,7 @@ window.addEventListener("load", function () {
     startButton.addEventListener("click", start);
 
     soundButton.addEventListener("click", soundOnOff);
-    document.addEventListener("keydown", jump);
+    document.addEventListener("keydown", actions);
 });
 
 const start = () => {
@@ -34,6 +39,9 @@ const start = () => {
         mario.classList.remove("hidden");
         mario.classList.add("entering-mario");
         mario.style.left = "10%";
+        setTimeout(() => {
+            mario.classList.remove("entering-mario");
+        }, 2000);
     }, 200);
 
     setTimeout(() => {
@@ -42,18 +50,39 @@ const start = () => {
 
     setTimeout(() => {
         pipeStart.classList.add("hidden");
+        pipe.classList.remove("hidden");
+        scores.classList.remove("hidden");
+        isStarted = true;
     }, 4000);
 };
 
-// const jump = () => {
-//     if (!mario.classList.contains("jump")) {
-//         mario.classList.add("jump");
+const runner = setInterval(() => {
+    const heightPipe = pipe.clientHeight;
+    const pipePosition = +window.getComputedStyle(pipe).left.replace("px", "");
 
-//         setTimeout(() => {
-//             mario.classList.remove("jump");
-//         }, jumpSpeed * 2);
-//     }
-// };
+    const marioPositionX = +window.getComputedStyle(mario).left.replace("px", "");
+    const marioPositionY = +window.getComputedStyle(mario).bottom.replace("px", "");
+    const marioWidth = mario.clientWidth;
+
+    if (marioPositionY <= heightPipe && pipePosition <= marioPositionX && pipePosition > marioWidth + 25) {
+        //TODO loser the game
+    }
+}, 1);
+
+const actions = (e) => {
+    if (isStarted) {
+        if (!mario.classList.contains("jump") && e.code === "Space") {
+            jumpAudio.play();
+            mario.classList.add("jump");
+
+            setTimeout(() => {
+                mario.classList.remove("jump");
+            }, jumpSpeed * 2);
+        } else if (e.code === "ControlLeft") {
+            //TODO fireball
+        }
+    }
+};
 
 const soundOnOff = () => {
     let icon = soundButton.querySelector("button");
